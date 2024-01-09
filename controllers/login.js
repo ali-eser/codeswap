4cfb181bd3e15aa6dbe3202ad8d270b84d303b0e
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-01-10 00:15:24 +0300

    Add logic for logging in

---
+++ controllers/login.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const loginRouter = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+loginRouter.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  const isCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
+
+  if (!(user && isCorrect)) {
+    return res.status(401).send("Invalid username or password")
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id
+  };
+
+  const token = jwt.sign(userForToken, process.env.SECRET);
+
+  res.status(200).send({ token, username: user.username, name: user.name });
+});
+
+module.exports = loginRouter;
