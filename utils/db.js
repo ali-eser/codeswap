c01f5f5980ae0b318e4f5450e0f5d2ec24ec3a3e
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-01-10 17:14:17 +0300

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,6 @@ const Sequelize = require("sequelize");
 const { DATABASE_URL } = require("./config");
 
 const sequelize = new Sequelize(DATABASE_URL, {
-  host: "localhost",
   dialect: "postgres"
 });
 
