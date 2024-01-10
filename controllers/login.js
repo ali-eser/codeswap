da5a6d223084ec9870d84c3a32e4cca4d9e0af90
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-01-10 04:57:44 +0300

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -17,7 +17,7 @@ loginRouter.post("/", async (req, res) => {
 
   const userForToken = {
     username: user.username,
-    id: user._id
+    id: user.id
   };
 
   const token = jwt.sign(userForToken, process.env.SECRET);
