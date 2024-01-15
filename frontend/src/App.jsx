123578bcedb52cfbec769b1892f823087d50469f
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-01-15 19:32:51 +0300

    Add reducer for user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { initUser } from "./reducers/userReducer";
 import LoginForm from "./components/LoginForm"; 
 import loginService from "./services/loginService";
 
 const App = () => {
+  const dispatch = useDispatch();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const user = useSelector(({user}) => user);
+
+  useEffect(() => {
+    const loggedUser = window.localStorage.getItem("loggedUser");
+    if (loggedUser) {
+      const user = JSON.parse(loggedUser);
+      dispatch(initUser(user))
+    }
+  }, [dispatch])
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       const user = await loginService.login({ username, password });
       window.localStorage.setItem("loggedUser", JSON.stringify(user));
+      dispatch(initUser(user));
     } catch (err) {
       console.log(err);
     }
   };
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    window.localStorage.clear();
+    dispatch(initUser(null));
+  };
+
   return (
     <div>
-      <LoginForm 
+      {!user && (
+        <LoginForm 
         username={username} 
         password={password}
         handleUsernameChange={({ target }) => setUsername(target.value)}
         handlePasswordChange={({ target }) => setPassword(target.value)}
         handleSubmit={handleLogin} 
       />
+      )}
+      {user && (
+        <div>
+          <h3>{user.username} logged in!</h3>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      )}
     </div>
   )
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 import App from './App.jsx'
 
+import userSlice from "./reducers/userReducer";
+
+const store = configureStore({
+  reducer: {
+    user: userSlice
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>,
+);
