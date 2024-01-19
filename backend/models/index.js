96cbc6e57956f4631f528bcc818a4d328aad2f4f
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-01-19 03:06:51 +0300

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,9 @@
 const User = require("./user");
 const Project = require("./project");
+const Follower = require("./follower");
+const Following = require("./following");
 
-User.hasMany(Project);
+User.hasMany(Project, { onDelete: 'CASCADE' });
 Project.belongsTo(User);
 
 User.sync({ alter: true });
