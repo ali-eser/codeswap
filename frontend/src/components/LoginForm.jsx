197ad2453887f2e28584e150ad703df79f450176
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-01-21 00:47:08 +0300

    Update login logic
    updated login logic to allow redirection after logging in. also a notification appears to notify a successful login.

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { initUser } from "../reducers/userReducer"
 import { defineNotification } from "../reducers/notificationReducer";
 import loginService from "../services/loginService";
 
 const LoginForm = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -16,6 +18,8 @@ const LoginForm = () => {
       const user = await loginService.login({ username, password });
       window.localStorage.setItem("loggedUser", JSON.stringify(user));
       dispatch(initUser(user));
+      navigate("/home");
+      dispatch(defineNotification(`${user.username} has successfully logged in`, 5));
     } catch (err) {
       dispatch(defineNotification(err.response.data, 5));
     }
