7cb3d55b871fdb43724b33e2acd71027f8709d2f
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-01-30 11:17:44 +0300

    Major rework to the welcome page

--- frontend/src/components/WelcomePage.jsx
+++ frontend/src/components/WelcomePage.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 const WelcomePage = () => {
+  const buttonStyle = {
+    marginLeft: "75px",
+    fontSize: "0.84em"
+  };
   return (
-    <div>
-      <h1>Welcome to Node Social App</h1>
+    <div className="welcomePage">
+      <h1 style={{ textShadow: "0px 10px 400px palevioletred", color: "palevioletred", fontSize: "2.2em" }}>CodeSwap</h1>
+      <div style={{display: "flex"}}>
+        <h1 style={buttonStyle}><Link to="/login">Login</Link></h1>
+        <h1 style={buttonStyle}><Link to="/signin">Sign in</Link></h1>
+      </div>
+
     </div>
   )
 };
