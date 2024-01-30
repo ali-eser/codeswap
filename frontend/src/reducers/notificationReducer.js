2b49e3c5da51677ae267f18a363ee7b5233d5230
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-01-30 11:17:25 +0300

    Update initialState as null

--- frontend/src/reducers/notificationReducer.js
+++ frontend/src/reducers/notificationReducer.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const notificationSlice = createSlice({
   name: "notification",
-  initialState: "",
+  initialState: null,
   reducers: {
     setNotification (state, action) {
       return action.payload
@@ -12,10 +12,10 @@ const notificationSlice = createSlice({
 
 export const { setNotification } = notificationSlice.actions;
 
-export const defineNotification = (text, time) => {
+export const defineNotification = (object, time) => {
   return dispatch => {
-    dispatch(setNotification(text));
-    setTimeout(() => dispatch(setNotification("")), time * 1000);
+    dispatch(setNotification(object));
+    setTimeout(() => dispatch(setNotification(null)), time * 1000);
   };
 };
 
