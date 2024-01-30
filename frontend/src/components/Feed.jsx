1dd6c1728f4230b8131e727358960dff068420fa
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-01-30 11:14:02 +0300

    Add classes

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -5,11 +5,18 @@ const Feed = () => {
   const projects = useSelector(({ projects }) => projects);
 
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className={"page-body"+ " " +"general-item"}>
+      <h1 className="title">Your Feed</h1>
       <ul>
-      {projects.map(project => (
-        <li key={project.id}><Link to={`/projects/${project.id}`} >{project.title}</Link> by <i>{project.user.username}</i></li>
+      {projects.map(p => (
+        <li key={p.id}>
+          <div>
+            <h3><Link to={`/projects/${p.id}`} >{p.title}</Link></h3>
+          </div>
+          <div>
+            by <Link to={`/users/${p.user.username}`}><i>{p.user.username}</i></Link> on {p.updatedAt.split("T")[0]}
+          </div>
+        </li>
       ))}
       </ul>
     </div>
