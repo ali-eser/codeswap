0e6686217701393d6fc2ec9fafd1db0ab8f66de0
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-02-20 22:08:04 +0300

    Add project images to project page

--- frontend/src/components/ProjectPage.jsx
+++ frontend/src/components/ProjectPage.jsx
@@ -1,24 +1,38 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, useMatch } from "react-router-dom";
+import projectService from "../services/projectService";
 
 const ProjectPage = () => {
   const projects = useSelector(({ projects }) => projects);
+  const [likeCount, setLikeCount] = useState(0);
   const match = useMatch("/projects/:id");
 
   const projectToShow = match
     ? projects.find(p => p.id === Number(match.params.id))
     : null
 
+  
+  console.log(projectToShow.imagePath);
+  const handleLike = async e => {
+    e.preventDefault();
+    console.log(projectToShow.id);
+    setLikeCount(projectToShow.likes.length);
+    await projectService.likeProject(projectToShow.id);
+  };
+
   return (
-    <div className="pageBody">
+    <div className={"page-body"+ " " +"general-item"}>
       {projectToShow && (
         <div>
-          <div>
-            <h1 className="title">{projectToShow.title}</h1>
-            <p>submitted by <Link to={`/users/${projectToShow.user.username}`}>{projectToShow.user.username}</Link> on {projectToShow.updatedAt.split("T")[0]}</p>
+          <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
+            <p className="title">{projectToShow.title}</p>
+            <p>submitted by <i><Link to={`/users/${projectToShow.user.username}`}>{projectToShow.user.username}</Link></i> on {projectToShow.updatedAt.split("T")[0]}</p>
           </div>
           <hr />
           <p>{projectToShow.description}</p>
+          <img src={"http://localhost:3001/" + projectToShow.imagePath} alt="Project Image" width="400px" />
+          <h5>{likeCount}<a href="" onClick={handleLike}> &#9829;</a></h5>
         </div>
       )}
     </div>
