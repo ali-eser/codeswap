33eef25755482cd559f2eceb5818191ad17a306f
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-04-30 19:47:09 +0300

    Update index.js for syncing likes and follows

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,14 +1,22 @@
 const User = require("./user");
 const Project = require("./project");
-const Follower = require("./follower");
-const Following = require("./following");
+const Like = require("./like");
+const Follow = require("./follow");
 
-User.hasMany(Project, { onDelete: 'CASCADE' });
+User.hasMany(Project, { onDelete: "CASCADE" });
 Project.belongsTo(User);
+
+User.belongsToMany(Project, { through: Like, as: "LikedProjects" });
+Project.belongsToMany(User, { through: Like, as: "LikedByUsers", onDelete: "CASCADE" });
+
+//User.belongsToMany(User, { through: Follow, as: "Follower", onDelete: "CASCADE" });
+//User.belongsToMany(User, { through: Follow, as: "Following", onDelete: "CASCADE" });
 
 User.sync({ alter: true });
 Project.sync({ alter: true });
+Like.sync({ alter: true });
+Follow.sync({ alter: true });
 
 module.exports = {
-  User, Project
+  User, Project, Like, Follow
 };
