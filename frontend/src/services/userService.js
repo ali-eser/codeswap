16bf7072f20a5ad4c04b1f20ab99a8bf2ee79843
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-04-30 19:52:50 +0300

    Update userService.js

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -2,10 +2,30 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:3001";
 const baseURL = "/api/users";
 
-const create = async (userToAdd) => {
+const create = async userToAdd => {
   const newUser = await axios.post(baseURL, userToAdd);
   console.log(newUser);
   return newUser;
 };
 
-export default { create };
+const getOne = async username => {
+  const user = await axios.get(`${baseURL}/${username}`);
+  return user;
+};
+
+const follow = async (followerId, followeeId) => {
+  const relation = await axios.put(`${baseURL}/${followerId}/follow/${followeeId}`);
+  return relation;
+};
+
+const getFollowings = async id => {
+  const followingsArray = await axios.get(`${baseURL}/getFollowings/${id}`);
+  return followingsArray;
+}
+
+const checkFollow = async id => {
+  const isFollowed = await axios.get(`${baseURL}/follow/${id}`);
+  return isFollowed;
+};
+
+export default { create, getOne, follow, getFollowings, checkFollow };
