ae5efc73ed8a0e8996e2f39c849208db4d2dc6cb
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-04-30 19:52:05 +0300

    Add follow button

--- frontend/src/components/ProfilePage.jsx
+++ frontend/src/components/ProfilePage.jsx
@@ -1,21 +1,60 @@
 import { useSelector } from "react-redux";
 import { Link, useMatch } from "react-router-dom";
+import userService from "../services/userService";
+import { useEffect, useState } from "react";
 
 const ProfilePage = () => {
-  const projects = useSelector(({ projects }) => projects);
-  const match = useMatch("/users/:username");
+  const { projects, loading } = useSelector(({ projects }) => projects);
+  const user = useSelector(({ user }) => user);
+  const [followText, setFollowText] = useState("Follow");
 
+  const match = useMatch("/users/:username");
   let userProjects = match
     ? projects.filter(p => p.user.username === match.params.username)
     : null
-
+  
   if (userProjects.length === 0) {
     userProjects = null;
   }
 
+  const ownProfile = user.username === match.params.username
+    ? true
+    : null
+
+  useEffect(() => {
+    const checkFollow = async () => {
+      const isFollowed = await userService.checkFollow(user.id);
+      setFollowText(isFollowed.data);
+    };
+    checkFollow();
+  });
+
+  const handleFollow = async (e) => {
+    e.preventDefault();
+    const currentUser = await userService.getOne(user.username);
+    const targetUser = await userService.getOne(match.params.username);
+    const relation = await userService.follow(currentUser.data.id, targetUser.data.id);
+
+    if (relation.data === "Followed") {
+      setFollowText("Unfollow");
+    } else {
+      setFollowText("Follow");
+    }
+  }
+
   return (
     <div className={"page-body"+ " " +"general-item"}>
-      <h1 className="title">{match.params.username}</h1> 
+      {!ownProfile && (
+        <div>
+          <h1 className="title">{match.params.username}</h1>
+          <button onClick={handleFollow}>{followText}</button>
+        </div>
+      )}
+      {ownProfile && (
+        <div>
+          <h1 className="title">{match.params.username}</h1>
+        </div>
+      )}
       {userProjects && (
         <div>
           <h3>Projects</h3>
