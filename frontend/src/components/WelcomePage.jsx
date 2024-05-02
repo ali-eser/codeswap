11278b0e3214931daecf9b9b54f07eb80e07ae7e
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-05-02 17:44:51 +0300

    Update login page text color

--- frontend/src/components/WelcomePage.jsx
+++ frontend/src/components/WelcomePage.jsx
@@ -7,7 +7,7 @@ const WelcomePage = () => {
   };
   return (
     <div className="welcomePage">
-      <h1 style={{ textShadow: "0px 10px 400px palevioletred", color: "palevioletred", fontSize: "2.2em" }}>CodeSwap</h1>
+      <h1 style={{ textShadow: "0px 10px 400px rgb(20, 47, 145)", color: "rgb(20, 47, 145)", fontSize: "2.2em" }}>CodeSwap</h1>
       <div style={{display: "flex"}}>
         <h1 style={buttonStyle}><Link to="/login">Login</Link></h1>
         <h1 style={buttonStyle}><Link to="/signin">Sign in</Link></h1>
