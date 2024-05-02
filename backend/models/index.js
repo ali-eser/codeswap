61c457f8fa8a449e90ca13ed7d5785eb0e3bfb7b
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-05-02 13:50:22 +0300

    Add comment model relations with other models

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,6 +2,7 @@ const User = require("./user");
 const Project = require("./project");
 const Like = require("./like");
 const Follow = require("./follow");
+const Comment = require("./comment");
 
 User.hasMany(Project, { onDelete: "CASCADE" });
 Project.belongsTo(User);
@@ -9,14 +10,15 @@ Project.belongsTo(User);
 User.belongsToMany(Project, { through: Like, as: "LikedProjects" });
 Project.belongsToMany(User, { through: Like, as: "LikedByUsers", onDelete: "CASCADE" });
 
-//User.belongsToMany(User, { through: Follow, as: "Follower", onDelete: "CASCADE" });
-//User.belongsToMany(User, { through: Follow, as: "Following", onDelete: "CASCADE" });
+Project.hasMany(Comment, { onDelete: "CASCADE" });
+Comment.belongsTo(Project);
 
 User.sync({ alter: true });
 Project.sync({ alter: true });
 Like.sync({ alter: true });
 Follow.sync({ alter: true });
+Comment.sync({ alter: true });
 
 module.exports = {
-  User, Project, Like, Follow
+  User, Project, Like, Follow, Comment
 };
