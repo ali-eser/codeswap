1e38573af8aaea4c6f710b66a498b4c16b7b1d80
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-09-04 19:37:05 +0300

    Check if user is logged in to skip splash screen

--- frontend/src/components/WelcomePage.jsx
+++ frontend/src/components/WelcomePage.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
+
 const WelcomePage = () => {
+  const navigate = useNavigate();
+  const user = useSelector(({ user }) => user);
+
+  useEffect(() => {
+      if (user) {
+        navigate("/home")
+      }
+  }, [navigate, user]);
+
   return (
     <div id="welcome-page">
       <h1 id="codeswap">CodeSwap</h1>
